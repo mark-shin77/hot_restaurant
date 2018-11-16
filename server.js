02375228b444aaa7e568b6745eb2558f741a8780
Max Wilets <mwilets@gmail.com>
2018-11-15 19:44:01 -0700

    start

---
+++ server.js
@@ -0,0 +1,16 @@
+// set up server
+//set up add table
+//set up reservation
+//set up waiting
+
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
